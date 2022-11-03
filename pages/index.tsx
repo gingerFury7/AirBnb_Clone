ddf6f8bb686fd887d30ea0f5664982997458e939
gingerFury7 <slavka2000@mail.ru>
2022-11-03 18:24:29 +0800

    header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import Header from "../components/Header";
 
 const Home: NextPage = () => {
   return (
@@ -10,8 +11,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Hello World!</h1>
-      {/** Header */}
+      <Header />
       {/** Banner */}
     </div>
   );
