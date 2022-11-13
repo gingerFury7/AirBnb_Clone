8093c0d0d00704a7418b12f777ca5e6ccf055a5a
gingerFury7 <slavka2000@mail.ru>
2022-11-13 14:42:21 +0800

    new custom class

--- pages/search.tsx
+++ pages/search.tsx
@@ -12,15 +12,8 @@ function Search() {
           <p className="text-xs ">300+ stays for 5 number of guests</p>
           <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in Mars</h1>
           <div className="hidden lg:inline-flex">
-            <p className="px-4 py-2 border rounded-full cursor-pointer hover:shadow-lg active:scale-95 active:bg-gray-100 transition transform duration-100 ease-out">
-              Cancellation Flexibility
-            </p>
-            <p className="px-4 py-2 border rounded-full cursor-pointer hover:shadow-lg active:scale-95 active:bg-gray-100 transition transform duration-100 ease-out">
-              Type of Place
-            </p>
-            <p className="px-4 py-2 border rounded-full cursor-pointer hover:shadow-lg active:scale-95 active:bg-gray-100 transition transform duration-100 ease-out">
-              Cancellation Flexibility
-            </p>
+            <p className="button">Cancellation Flexibility</p>
+            <p className="button">Type of Place</p>
           </div>
         </section>
       </main>
